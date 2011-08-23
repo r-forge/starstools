31adbe3a5882876f1cad14982c5ce6f2afcc4e42
jgrn307 (Dr. ) <jgreenberg@unr.edu>
2011-08-23 23:20:10 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/starstools@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/STARStools/R/tile_raster.R
@@ -0,0 +1,22 @@
+# TODO: Add comment
+# 
+# Author: jonathan
+###############################################################################
+
+
+tile_raster=function(raster,ps=c(256,256),overlap=c(0,0),corner="ul")
+{
+	if(!inherits(rasters,"Raster"))
+	{
+		print("raster must be a single raster of class inheriting Raster (e.g. raster, stack or brick).")
+		return()
+	}
+	
+	if(length(ps)==1)
+	{
+		ps=c(ps,ps)
+	}
+	
+	raster_extent=extent(raster)
+	
+}
